1b0e15c4a3ecef39e053757cc278bd6a14768e5c
Ângelo Victor <angelovictor.ti@gmail.com>
2022-12-08 11:06:21 -0300

    Update CardMB.js
    Adjustments in the list of milk banks for a dynamic way

--- pages/MilkBanks/CardMB.js
+++ pages/MilkBanks/CardMB.js
@@ -25,155 +25,37 @@ export default function CardMB({ navigation }) {
 
   return (
     <ScrollView backgroundColor={tema}>
-      <Box
-        alignItems="center"
-        marginTop="4"
-        marginBottom="1"
-        backgroundColor={tema}
-      >
-        <Pressable
-          rounded="8"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="coolGray.300"
-          maxW="96"
-          shadow="3"
-          bg="coolGray.100"
-          p="5"
-        >
-          <Box>
-            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-              {milkBank[0]?.name_bank}{" "}
-            </Text>
-            <Text mt="2" fontSize="sm" color="coolGray.700">
-              {milkBank[0]?.address}{" "}
-            </Text>
-            <Flex>
-              <TouchableOpacity onPress={"Show"}>
+      <Box alignItems="center" my="5">
+        {milkBank.map((item, index) => (
+          <Pressable
+            onPress={() => console.log("I'm Pressed")}
+            rounded="8"
+            overflow="hidden"
+            borderWidth="1"
+            borderColor="coolGray.300"
+            maxW="96"
+            shadow="3"
+            bg={(index % 2 == 0) ? "coolGray.100" : "coolGray.200"}
+            p="5"
+            my="2.5"
+            w="full"
+            key={item?.milk_bank_id}
+          >
+            <Box>
+              <Text color="coolGray.800" fontWeight="medium" fontSize="xl">
+                {item?.name_bank}
+              </Text>
+              <Text mt="2" fontSize="sm" color="coolGray.700">
+                {item?.address}
+              </Text>
+              <Flex>
                 <Text mt="2" fontSize={12} fontWeight="medium" color="#FF5F24">
                   Mais Informações
                 </Text>
-              </TouchableOpacity>
-            </Flex>
-          </Box>
-        </Pressable>
-      </Box>
-
-      <Box
-        alignItems="center"
-        marginTop="2"
-        marginBottom="1"
-        backgroundColor={tema}
-      >
-        <Pressable
-          onPress={() => console.log("I'm Pressed")}
-          rounded="8"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="coolGray.300"
-          maxW="96"
-          shadow="3"
-          bg="#EEEFF8"
-          p="5"
-        >
-          <Box>
-            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-              {milkBank[1]?.name_bank}{" "}
-            </Text>
-            <Text mt="2" fontSize="sm" color="coolGray.700">
-              {milkBank[1]?.address}{" "}
-            </Text>
-            <Flex>
-              <Text mt="2" fontSize={12} fontWeight="medium" color="#FF5F24">
-                Mais Informações
-              </Text>
-            </Flex>
-          </Box>
-        </Pressable>
-      </Box>
-
-      <Box alignItems="center" marginTop="2" marginBottom="1">
-        <Pressable
-          onPress={() => console.log("I'm Pressed")}
-          rounded="8"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="coolGray.300"
-          maxW="96"
-          shadow="3"
-          bg="coolGray.100"
-          p="5"
-        >
-          <Box>
-            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-              {milkBank[2]?.name_bank}{" "}
-            </Text>
-            <Text mt="2" fontSize="sm" color="coolGray.700">
-              {milkBank[2]?.address}{" "}
-            </Text>
-            <Flex>
-              <Text mt="2" fontSize={12} fontWeight="medium" color="#FF5F24">
-                Mais Informações
-              </Text>
-            </Flex>
-          </Box>
-        </Pressable>
-      </Box>
-
-      <Box alignItems="center" marginTop="2" marginBottom="1">
-        <Pressable
-          onPress={() => console.log("I'm Pressed")}
-          rounded="8"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="coolGray.300"
-          maxW="96"
-          shadow="3"
-          bg="#EEEFF8"
-          p="5"
-        >
-          <Box>
-            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-              {milkBank[3]?.name_bank}{" "}
-            </Text>
-            <Text mt="2" fontSize="sm" color="coolGray.700">
-              {milkBank[3]?.address}{" "}
-            </Text>
-            <Flex>
-              <Text mt="2" fontSize={12} fontWeight="medium" color="#FF5F24">
-                Mais Informações
-              </Text>
-            </Flex>
-          </Box>
-        </Pressable>
-      </Box>
-
-      <Box alignItems="center" marginTop="2" marginBottom="1">
-        <Pressable
-          onPress={() => console.log("I'm Pressed")}
-          rounded="8"
-          overflow="hidden"
-          borderWidth="1"
-          borderColor="coolGray.300"
-          maxW="96"
-          shadow="3"
-          bg="coolGray.100"
-          p="5"
-        >
-          <Box>
-            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-              {milkBank[4]?.name_bank}{" "}
-            </Text>
-            <Text mt="2" fontSize="sm" color="coolGray.700">
-              {milkBank[4]?.address}{" "}
-            </Text>
-            <Flex>
-              <Text mt="2" fontSize={12} fontWeight="medium" color="#FF5F24">
-                Mais Infomações
-              </Text>
-            </Flex>
-          </Box>
-        </Pressable>
+              </Flex>
+            </Box>
+          </Pressable>
+        ))}
       </Box>
       <Button
         fontSize={12}
